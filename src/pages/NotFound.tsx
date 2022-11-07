4074299edc90326c9cb5c611238ca850e794cc9a
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-11-07 19:16:20 +0900

    [Add] NotFound.tsx

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+    return <div>Not Found, 404</div>;
+};
+
+export default NotFound;
--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
+import NotFound from "../pages/NotFound";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Main />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     );
